6ad99cc7c373519f5fdc60da2c9d061a55aa23bb
develon2015 <302615249@qq.com>
2023-07-05 14:56:21 +0800

    引入脚手架Scaffold、自定义TabRow指标indicator颜色

--- app/src/main/java/app/remote_bind/AppUI.kt
+++ app/src/main/java/app/remote_bind/AppUI.kt
@@ -1,45 +1,100 @@
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
 val titles = listOf<String>("配置列表", "服务器列表")
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun AppUI() {
     val pagerState = rememberPagerState()
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text(text = titles[pagerState.currentPage])
+            }, Modifier.background(color = Color.Blue))
+        },
+        content = { innerPadding -> AppContent(pagerState, innerPadding) },
+        bottomBar = { AppBottomBar(pagerState) }
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun AppBottomBar(pagerState: PagerState) {
     val coroutineScope = rememberCoroutineScope()
-    Column {
+    val height = 48.dp
+    val containerColor = Color.Black
+    BottomAppBar(
+        contentPadding = PaddingValues(0.dp),
+        containerColor = containerColor,
+        modifier = Modifier.height(height)
+    ) {
         TabRow(
             selectedTabIndex = pagerState.currentPage,
-            modifier = Modifier.fillMaxWidth(),
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    color = Color.Red,
+                    modifier = Modifier.tabIndicatorOffset(tabPositions[pagerState.currentPage])
+                )
+            },
+            containerColor = containerColor,
+            modifier = Modifier.height(height)
         ) {
             titles.forEachIndexed { index, value ->
-                Tab(text = { Text(value) }, selected = pagerState.currentPage == index, onClick = {
-                    coroutineScope.launch {
-                        // 同步 HorizontalPager 的状态
-                        pagerState.animateScrollToPage(index)
-                    }
-                })
+                Tab(
+                    text = { Text(text = value, color = Color.White) },
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        coroutineScope.launch {
+                            // 同步 HorizontalPager 的状态
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    modifier = Modifier
+                        .wrapContentHeight()
+                        .background(color = containerColor)
+                )
             }
         }
-        HorizontalPager(state = pagerState, pageCount = titles.size) { page ->
-            val text = titles[page % titles.size]
-            Column(modifier = Modifier.background(color = Color.Cyan).fillMaxSize()) {
-                Text(text)
-            }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun AppContent(pagerState: PagerState, innerPadding: PaddingValues) {
+    HorizontalPager(
+        state = pagerState, pageCount = titles.size, modifier = Modifier
+    ) { pageIndex ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Text(text = titles[pageIndex])
         }
     }
 }
