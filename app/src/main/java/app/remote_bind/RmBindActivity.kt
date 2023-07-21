186a07198c4baa1a9e23d58e5801b7e2c3496132
develon2015 <302615249@qq.com>
2023-07-21 16:27:56 +0800

    Make Activity to be singleInstance

--- app/src/main/java/app/remote_bind/RmBindActivity.kt
+++ app/src/main/java/app/remote_bind/RmBindActivity.kt
@@ -1,10 +1,12 @@
 package app.remote_bind
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import app.remote_bind.ui.AppUI
 import app.remote_bind.ui.theme.RemoteBindTheme
+import lib.log
 
 class RmBindActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,10 @@ class RmBindActivity : ComponentActivity() {
                 )
             }
         }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        log.d("复用 Activity 实例")
     }
 }
